1016e3e8f4e4d9910b2a7c1da398664f3131c226
Vladimir Kukushkin <kukushkin.v@gmail.com>
2020-03-31 00:51:11 +0300

    Refactored get_transcription in more intelligent way

--- lingtools.py
+++ lingtools.py
@@ -15,9 +15,9 @@ def squeeze_sibilants(word):
     return word
 
 
-class Node:
-    def __init__(self, index=None, letter=None):
-        self.index = index
+class Letter:
+    def __init__(self, letter=None):
+        self.index = None
         self.letter = letter
         self.phoneme = letter
         self.type = None
@@ -27,6 +27,42 @@ class Node:
         self.prev = None
         self.next = None
         self.yot = None
+        self.vowel_num = None
+
+
+class Word:
+    def __init__(self, word, stressed_vowel):
+        self.length = len(word)
+        self.pointer = self.length - 1
+        self.stressed_vowel = stressed_vowel
+        self.word = [Letter(letter) for letter in word]
+
+        vowel_num = 0
+        for i, l in enumerate(reversed(self.word)):
+            l.phoneme = l.letter
+            l.index = i
+            if l.letter in 'аеёиоуыэюя':
+                l.type = 'v'
+                vowel_num += 1
+                l.vowel_num = vowel_num
+                l.stressed = (vowel_num == self.stressed_vowel)
+            elif l.letter in 'бвгджзйклмнпрстфхцчшщ':
+                l.type = 'c'
+            elif l.letter in 'ьъ':
+                l.type = 'm'
+            else:
+                raise ValueError('Unknown character ' + l.letter)
+
+            l.prev = self.word[self.length - i] if i > 0 else None
+            l.next = self.word[self.length - i - 2] if i < self.length - 1 else None
+
+    def iterate_backward(self):
+        for i in range(self.length - 1, -1, -1):
+            yield self.word[i]
+
+    def __iter__(self):
+        for letter in self.word:
+            yield letter
 
 
 class LingTools:
@@ -72,224 +108,187 @@ class LingTools:
         syllables.append(''.join(current_syllable['syl']))
         return syllables
 
-    def _get_nodes(self, word, stressed_vowel):
-        rword = list(reversed(word))
-        prev_item = Node(0, rword[0])
-        if rword[0] in self.vowels:
-            prev_item.type = 'v'
-        elif rword[0] in self.consonants:
-            prev_item.type = 'c'
-        elif rword[0] in ['ь', 'ъ']:
-            prev_item.type = 'm'
-        else:
-            raise ValueError('Unknown token ' + rword[0])
-        head = prev_item
-        n_vowel = 1 if rword[0] in self.vowels else 0
-        for i, letter in enumerate(rword[1:]):
-            new_item = Node(i + 1, letter)
-            if letter in self.vowels:
-                new_item.type = 'v'
-                n_vowel += 1
-                new_item.stressed = (n_vowel == stressed_vowel)
-            elif letter in self.consonants:
-                new_item.type = 'c'
-            elif letter in ['ь', 'ъ']:
-                new_item.type = 'm'
-            else:
-                raise ValueError('Unknown token "' + letter + '"')
-            prev_item.next = new_item
-            new_item.prev = prev_item
-            prev_item = new_item
-        return head
+    def _insert_yot(self, word, item, position, letter=None):
+        if letter is None:
+            letter = item.letter
 
-    def _insert_yot(self, p, letter, position, length):
-        p.phoneme = self.vowel_phonemes.loc[letter][position]
-        if p.letter in ['ю', 'е', 'ё', 'я']:
-            if p.index == length - 1:
-                p.yot = True
-            elif p.next is not None and (p.next.type == 'v' or p.next.letter in ['ь', 'ъ']):
-                p.yot = True
+        item.phoneme = self.vowel_phonemes.loc[letter][position]
+        if item.letter in ['ю', 'е', 'ё', 'я']:
+            if item.index == word.length - 1:
+                item.yot = True
+            elif item.next is not None and (item.next.type == 'v' or item.next.letter in ['ь', 'ъ']):
+                item.yot = True
                 if 'vn' in position:
-                    p.phoneme = self.vowel_phonemes.loc[letter]['vn_hard']
-        elif p.letter in ['и', 'о'] and p.next is not None and p.next.letter == 'ь':
-            p.yot = True
+                    item.phoneme = self.vowel_phonemes.loc[letter]['vn_hard']
+        elif item.letter in ['и', 'о'] and item.next is not None and item.next.letter == 'ь':
+            item.yot = True
             if 'vn' in position:
-                p.phoneme = self.vowel_phonemes.loc[letter]['vn_hard']
+                item.phoneme = self.vowel_phonemes.loc[letter]['vn_hard']
 
-    def _get_vowel_phoneme(self, p, vowel_num, stressed_syllable, length):
-        letter = p.letter
+    def _set_vowel_phoneme(self, word, item):
+        letter = item.letter
 
         # TODO: ударный Е может давать Э: тире -> тирэ, но горе -> гаре
-        if p.stressed is True:
-            if p.letter not in ['а', 'о', 'у', 'ы'] and p.next is not None and p.next.type == 'c':
+        if item.stressed is True:
+            if item.letter not in ['а', 'о', 'у', 'ы'] and item.next is not None and item.next.type == 'c':
                 # e.g.: мяч, лист
-                if p.letter != 'э' and p.next.letter not in ['ш', 'ж', 'ц']:
-                    p.next.soft = True
+                if item.letter != 'э' and item.next.letter not in ['ш', 'ж', 'ц']:
+                    item.next.soft = True
                 # e.g.: желчь, шест
-                elif p.letter in ['э', 'е'] and p.next.letter in ['ш', 'ж', 'ц']:
+                elif item.letter in ['э', 'е'] and item.next.letter in ['ш', 'ж', 'ц']:
                     letter = 'э'
                 # e.g.: жир, цирк
-                elif p.letter == 'и':
+                elif item.letter == 'и':
                     letter = 'ы'
-            self._insert_yot(p, letter, 'V', length)
+            self._insert_yot(word, item, 'V', letter)
 
         # начало
-        elif p.index == length - 1:
-            self._insert_yot(p, letter, '#', length)
+        elif item.index == word.length - 1:
+            self._insert_yot(word, item, '#', letter)
 
         # первый предударный
-        elif vowel_num == stressed_syllable + 1:
+        elif item.vowel_num == word.stressed_vowel + 1:
             # e.g.: живот, шумок
-            if p.next is not None and p.next.letter in ['ц', 'ж', 'ш']:
-                p.phoneme = self.vowel_phonemes.loc[letter]['v1_sh']
+            if item.next is not None and item.next.letter in ['ц', 'ж', 'ш']:
+                item.phoneme = self.vowel_phonemes.loc[letter]['v1_sh']
             # e.g.: ведро, вьюнок, связно́й, привет
             elif letter in ['е', 'ё', 'и', 'ю', 'я']:
-                p.phoneme = self.vowel_phonemes.loc[letter]['v1_soft']
-                p.next.soft = True
+                item.phoneme = self.vowel_phonemes.loc[letter]['v1_soft']
+                item.next.soft = True
             # e.g.: чепец, щавель, майдан
-            elif p.next is not None and p.next.letter in ['щ', 'ч', 'й']:
-                p.phoneme = self.vowel_phonemes.loc[letter]['v1_soft']
+            elif item.next is not None and item.next.letter in ['щ', 'ч', 'й']:
+                item.phoneme = self.vowel_phonemes.loc[letter]['v1_soft']
             # e.g.: душа, тосол, наказ, дымо́к
             else:
-                p.phoneme = self.vowel_phonemes.loc[letter]['v1_hard']
+                item.phoneme = self.vowel_phonemes.loc[letter]['v1_hard']
 
         # второй предударный
-        elif vowel_num >= stressed_syllable + 2:
+        elif item.vowel_num >= word.stressed_vowel + 2:
             # e.g.: шоколад, циферблат, журавля
-            if p.next is not None and p.next.letter in ['ц', 'ж', 'ш']:
-                p.phoneme = self.vowel_phonemes.loc[letter]['v2_hard']
-            elif p.next is not None and p.next.type == 'v':
+            if item.next is not None and item.next.letter in ['ц', 'ж', 'ш']:
+                item.phoneme = self.vowel_phonemes.loc[letter]['v2_hard']
+            elif item.next is not None and item.next.type == 'v':
                 # e.g.: иерархичный, эякуляция
                 if letter in ['е', 'ё', 'и', 'ю', 'я']:
-                    p.phoneme = self.vowel_phonemes.loc[letter]['v1_soft']
+                    item.phoneme = self.vowel_phonemes.loc[letter]['v1_soft']
                 # e.g.: аэростат, ионизация, аудиенция
                 else:
-                    p.phoneme = self.vowel_phonemes.loc[letter]['v1_hard']
+                    item.phoneme = self.vowel_phonemes.loc[letter]['v1_hard']
             # e.g.: сенокос, динамит
             elif letter in ['е', 'ё', 'и', 'ю', 'я']:
-                p.phoneme = self.vowel_phonemes.loc[letter]['v2_soft']
-                p.next.soft = True
+                item.phoneme = self.vowel_phonemes.loc[letter]['v2_soft']
+                item.next.soft = True
             # e.g.: щупловатый, йодоформ, чаепитие
-            elif p.next is not None and p.next.letter in ['щ', 'ч', 'й']:
-                p.phoneme = self.vowel_phonemes.loc[letter]['v2_soft']
+            elif item.next is not None and item.next.letter in ['щ', 'ч', 'й']:
+                item.phoneme = self.vowel_phonemes.loc[letter]['v2_soft']
             # e.g.: гуталин, барабан, ломоносов, крысолов
             else:
-                p.phoneme = self.vowel_phonemes.loc[letter]['v2_hard']
+                item.phoneme = self.vowel_phonemes.loc[letter]['v2_hard']
 
         # заударные
-        elif vowel_num < stressed_syllable:
+        elif item.vowel_num < word.stressed_vowel:
             # e.g.: бежевый, ницца, ковшик
-            if p.next is not None and p.next.letter in ['ц', 'ж', 'ш']:
-                self._insert_yot(p, letter, 'vn_hard', length)
+            if item.next is not None and item.next.letter in ['ц', 'ж', 'ш']:
+                self._insert_yot(word, item, 'vn_hard')
             elif letter in ['е', 'ё', 'и', 'ю', 'я']:
                 # e.g.: дует, аист, заяц
-                if vowel_num == stressed_syllable - 1 and p.next is not None and p.next.type == 'v':
-                    self._insert_yot(p, letter, 'vn_soft', length)
+                if item.vowel_num == word.stressed_vowel - 1 and item.next is not None and item.next.type == 'v':
+                    self._insert_yot(word, item, 'vn_soft')
                 # e.g.: море, гири, бурые, делаю
                 else:
-                    p.phoneme = self.vowel_phonemes.loc[letter]['vn_soft']
-                p.next.soft = True
+                    item.phoneme = self.vowel_phonemes.loc[letter]['vn_soft']
+                item.next.soft = True
             # e.g.: общую, мачо, огайо
-            elif p.next is not None and p.next.letter in ['щ', 'ч', 'й']:
-                p.phoneme = self.vowel_phonemes.loc[letter]['vn_soft']
+            elif item.next is not None and item.next.letter in ['щ', 'ч', 'й']:
+                item.phoneme = self.vowel_phonemes.loc[letter]['vn_soft']
             else:
                 # e.g.: анчоус, какао
-                if vowel_num == stressed_syllable - 1 and p.next is not None and p.next.type == 'v':
-                    self._insert_yot(p, letter, 'vn_hard', length)
+                if item.vowel_num == word.stressed_vowel - 1 and item.next is not None and item.next.type == 'v':
+                    self._insert_yot(word, item, 'vn_hard')
                 # e.g.: руку, тихо, дерево, мачеха
                 else:
-                    p.phoneme = self.vowel_phonemes.loc[letter]['vn_hard']
+                    item.phoneme = self.vowel_phonemes.loc[letter]['vn_hard']
 
-    def _get_consonant_phoneme(self, p, vcd):
-        if p.letter in ['ч', 'ш', 'щ', 'ж']:
-            p.phoneme = self.consonant_phonemes.loc[p.letter]['hard']
-            if p.soft is True:
-                p.soft = False
-            if p.letter in ['ч', 'щ']:
-                if p.voice is not None:
-                    p.voice = None
-        if p.voice is True:
-            p.phoneme = self.consonant_phonemes.loc[p.letter]['voice']
-        elif p.voice is False:
-            p.phoneme = self.consonant_phonemes.loc[p.letter]['no_voice']
+    def _set_consonant_phoneme(self, item, vcd):
+        if item.letter in ['ч', 'ш', 'щ', 'ж']:
+            item.phoneme = self.consonant_phonemes.loc[item.letter]['hard']
+            if item.soft is True:
+                item.soft = False
+            if item.letter in ['ч', 'щ']:
+                if item.voice is not None:
+                    item.voice = None
+        if item.voice is True:
+            item.phoneme = self.consonant_phonemes.loc[item.letter]['voice']
+        elif item.voice is False:
+            item.phoneme = self.consonant_phonemes.loc[item.letter]['no_voice']
 
         if vcd is True:
-            if p.prev is None:
-                p.phoneme = self.consonant_phonemes.loc[p.letter]['voiced']
+            if item.prev is None:
+                item.phoneme = self.consonant_phonemes.loc[item.letter]['voiced']
         else:
-            if p.prev is None:
-                if p.next is not None:
-                    p.next.voice = False
-                if self.phoneme_properties.loc[p.phoneme]['vcd'] == '+':
-                    p.phoneme = self.consonant_phonemes.loc[p.letter]['no_voice']
+            if item.prev is None:
+                if item.next is not None:
+                    item.next.voice = False
+                if self.phoneme_properties.loc[item.phoneme]['vcd'] == '+':
+                    item.phoneme = self.consonant_phonemes.loc[item.letter]['no_voice']
 
-        if p.next is not None:
+        if item.next is not None:
             # оглушение следующих
-            if p.phoneme in self.phoneme_properties.index\
-                    and self.phoneme_properties.loc[p.phoneme]['vcd'] == '-'\
-                    and p.next is not None:
-                if p.next.phoneme in self.phoneme_properties.index\
-                        and self.phoneme_properties.loc[p.next.phoneme]['son'] == '-':
-                    p.next.voice = False
+            if item.phoneme in self.phoneme_properties.index\
+                    and self.phoneme_properties.loc[item.phoneme]['vcd'] == '-'\
+                    and item.next is not None:
+                if item.next.phoneme in self.phoneme_properties.index\
+                        and self.phoneme_properties.loc[item.next.phoneme]['son'] == '-':
+                    item.next.voice = False
             # озвончение следующих
-            elif p.phoneme in self.phoneme_properties.index\
-                    and self.phoneme_properties.loc[p.phoneme]['son'] == '-'\
-                    and p.phoneme not in ['в', "в’"]\
-                    and self.phoneme_properties.loc[p.phoneme]['vcd'] == '+':
-                p.next.voice = True
+            elif item.phoneme in self.phoneme_properties.index\
+                    and self.phoneme_properties.loc[item.phoneme]['son'] == '-'\
+                    and item.phoneme not in ['в', "в’"]\
+                    and self.phoneme_properties.loc[item.phoneme]['vcd'] == '+':
+                item.next.voice = True
 
-        if p.prev is not None:
-            if p.prev.phoneme == 'к' and p.letter == 'г':
-                p.phoneme = 'х'
-            elif p.prev.letter in [p.letter, p.letter + "’"]:
-                p.phoneme = ''
+        if item.prev is not None:
+            if item.prev.phoneme == 'к' and item.letter == 'г':
+                item.phoneme = 'х'
+            elif item.prev.letter in [item.letter, item.letter + "’"]:
+                item.phoneme = ''
 
     @staticmethod
-    def _get_transcription_from_structure(p):
-        res = []
-        while p is not None:
-            res.append(p.phoneme)
-            p = p.next
-        return res
+    def _get_transcription_from_structure(word):
+        return [letter.phoneme for letter in word]
 
     def get_transcription(self, word, vcd=False):
         stressed_syllable = self.dict[word]['stressed_syllable']
         word = squeeze_sibilants(word)
-        head = self._get_nodes(word, stressed_syllable)
-        vowel_num = 0
-        pointer = head
-        length = len(word)
+        word = Word(word, stressed_syllable)
         special = False
-        while pointer is not None:
-            if pointer.type == 'v':
-                vowel_num += 1
-                self._get_vowel_phoneme(pointer, vowel_num, stressed_syllable, length)
-                if pointer.yot is True and pointer.index == 0 and pointer.stressed is False:
+        for item in word.iterate_backward():
+            if item.type == 'v':
+                self._set_vowel_phoneme(word, item)
+                if item.yot is True and item.index == 0 and item.stressed is False:
                     special = True
-            elif pointer.type == 'm':
-                pointer.phoneme = ''
-                if pointer.letter == 'ь':
-                    pointer.next.soft = True
-            elif pointer.type == 'c':
-                if pointer.letter == 'й':
-                    pointer.phoneme = 'ṷ'
-                self._get_consonant_phoneme(pointer, vcd)
+            elif item.type == 'm':
+                item.phoneme = ''
+                if item.letter == 'ь':
+                    item.next.soft = True
+            elif item.type == 'c':
+                if item.letter == 'й':
+                    item.phoneme = 'ṷ'
+                self._set_consonant_phoneme(item, vcd)
             else:
-                raise ValueError('Unknown character ' + pointer.letter)
+                raise ValueError('Unknown character ' + item.letter)
 
-            if pointer.yot is True:
+            if item.yot is True:
                 if special:
-                    pointer.phoneme = pointer.phoneme + 'ṷ'
+                    item.phoneme = item.phoneme + 'ṷ'
                     special = False
                 else:
-                    pointer.phoneme = pointer.phoneme + 'j'
+                    item.phoneme = item.phoneme + 'j'
+        return self._get_transcription_from_structure(word)
 
-            pointer = pointer.next
-        return self._get_transcription_from_structure(head)
-
-
+#
 # lt = LingTools()
-# print(lt.get_transcription('мама'))
+# print(lt.get_transcription('тяжесть'))
 
 import sys
 if __name__ == '__main__':
