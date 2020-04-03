7089e639d899e2a995dd8fe7b9bef10e1abfb1e4
pavlov200912 <pavlov200912@gmail.com>
2020-04-03 21:50:34 +0300

    some beam_search tests

---
+++ tests/test_beam_search.py
@@ -0,0 +1,29 @@
+import unittest
+from rhythm.rhythm_handler import RhythmHandler
+import numpy as np
+import logging
+import model.beam_search as bm
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)
+
+    def test_lol_kek_cheburek(self):
+        seq = ['лол', 'кек', 'чебурек', 'кот']
+
+        def probs(lst):
+            if not lst:
+                return [0.9, 0.0, 0.0, 0.1]
+            if lst == ['лол']:
+                return [.0, .7, .2, .1]
+            if lst == ['лол', 'кек']:
+                return [.1, .2, .5, 0.2]
+            return [.25, .25, .25, .25]
+
+        assert(bm.beam_search(iterations=3, candidates=seq, get_prob_by_seq=probs, seq=[])[-1]
+            == ['лол', 'кек', 'чебурек'])
+
+
+if __name__ == '__main__':
+    unittest.main()
