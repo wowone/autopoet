f970a0ed98db25b40e203eeeee08ab100946bc2c
pavlov200912 <pavlov200912@gmail.com>
2020-04-03 21:49:50 +0300

    generate_seq rewrited

--- model/poem_generator.py
+++ model/poem_generator.py
@@ -10,6 +10,8 @@ import numpy as np
 import model.language_model as lang
 from tokenizer import MyTokenizer
 import rhythm.rhythm_handler as rh
+from model.beam_search import beam_search
+
 
 
 def generate_probas(preds, temperature):
@@ -23,7 +25,7 @@ def generate_probas(preds, temperature):
     return preds
 
 
-def get_word(prediction, mask, temperature=1):
+def get_word(prediction, mask, temperature=1.):
     p = generate_probas(prediction, temperature)
     mmask = np.multiply(p, mask)
     # print(np.max(mmask))
@@ -31,65 +33,50 @@ def get_word(prediction, mask, temperature=1):
     # return tokenizer.id_to_word[np.argmax(p)]
 
 
-def generate_seq2(model, seq_length, seed_text, lines_len, masks, handler, words, footness=4):
-    result = list()
-    in_text = seed_text
-    # generate a fixed number of words
-
-    # TODO: Footness isn't just count of words, but maximum count of stresses in one line
-
-    index = 0
+def generate_sequence(model, seq_len, seed, lines_len, stress_masks, rhythm_handler, words, footness,
+                      rhyme_module, rhyme_dict):
+    current_text = seed
     shift = 0
-    line_index = 0
-    lines_count = 0
-    mask_default = np.array([1] * len(words))
-    rhyme_word = ["", ""]
+    last_words = {-1: ""}
+    for index in range(rhyme_module):
+        last_words[index] = ""
+    poem = ""
+    neutral_mask = np.array([1] * len(words))
 
-    while (lines_count != lines_len):
-        line_index = index % footness
-        change_line = ((line_index + 1) % footness) == 0
+    for line_index in range(lines_len):
+        stress_count = 0
+        shift = 0
+        while stress_count != footness:
+            mask_stress = stress_masks[shift]
+            # While generation is straightforward
+            if footness - 1 == stress_count:
+                mask_rhyme = rhyme_mask(last_words[rhyme_dict[line_index % rhyme_module]], words)
+            else:
+                mask_rhyme = neutral_mask
+            mask = np.multiply(mask_stress, mask_rhyme)
 
-        mask = masks[shift]
-        if change_line and (lines_count % 4 == 2 or lines_count % 4 == 3):
-            # print(predicted_word, lines_count, rhyme_word[lines_count % 4 - 2])
-            mask_r = rhyme_mask(rhyme_word[lines_count % 4 - 2], words)
-        else:
-            mask_r = mask_default
 
-        mask = np.multiply(mask, mask_r)
+            encoded = tokenizer.text_to_seq([current_text])[0]
+            # TODO: Padded with zeros! Horrible solution! REDO!!!
+            encoded = pad_sequences([encoded], maxlen=seq_len, truncating='pre')
 
-        encoded = tokenizer.text_to_seq([in_text])[0]
-        # truncate sequences to a fixed length
+            prediction = model.predict(encoded)[0]
 
-        encoded = pad_sequences([encoded], maxlen=seq_length, truncating='pre')
+            predicted_word = get_word(prediction, mask, temperature=0.7)
+            poem += predicted_word+ ' '
+            current_text += ' ' + predicted_word
 
-        # predict probabilities for each word
+            shift = rhythm_handler.get_next_shift(predicted_word, shift)
 
-        prediction = model.predict(encoded)[0]
-
-        predicted_word = get_word(prediction, mask)
-        shift = handler.get_next_shift(predicted_word, shift)
-
-        if change_line and (lines_count % 4 == 0 or lines_count % 4 == 1):
-            rhyme_word[lines_count % 4] = predicted_word
-            # print(f"Rhyme to: {predicted_word}")
-
-        if change_line:
-            # rhyme_word = predicted_word
-
-            shift = 0
-            lines_count += 1
-
-        # print(f"predicted_word: {predicted_word} next_shift is: {shift} stressed syllable is:\
-        #      {handler.get_stress_syllable(predicted_word)}")
-
-        result.append(predicted_word)
-
-        in_text += ' ' + predicted_word
-        index += 1
-    result = [result[i:i + footness] for i in range(0, len(result), footness)]
-
-    return '\n'.join(list(map(lambda l: ' '.join(l), result)))
+            # Check if stressed
+            if rhythm_handler.get_stress_syllable(predicted_word) != -1:
+                stress_count += 1
+            if footness == stress_count:
+                last_words[line_index % rhyme_module] = predicted_word
+                poem += '\n'
+        if line_index % rhyme_module == rhyme_module - 1:
+            poem += '\n'
+    return poem
 
 
 def generate_word_list(tokenizer_):
@@ -114,6 +101,8 @@ def longest_common_suffix(a, b):
 
 # Will be replaced with more complicated
 def rhyme_mask(word, words):
+    if word == "":
+        return np.array([1] * len(words))
     mask = np.array([0] * len(words))
     for i, w in enumerate(words):
         if longest_common_suffix(w, word) >= 2:
@@ -163,13 +152,29 @@ if __name__ == '__main__':
     # Words dictionary of dataset
     words = generate_word_list(tokenizer)
 
-    rhythm_handler = rh.RhythmHandler(get_stress, rhythm=(1, 2))
+    rhythm_handler = rh.RhythmHandler(get_stress, rhythm=(0, 2))
     rhythm_handler.generate_masks()
     masks = rhythm_handler.get_masks_for_words(words)
 
     # must contain only words from dataset
+    print(tokenizer.id_to_word[0])
 
-    seed = ""
-    result = generate_seq2(model, 32, seed, 4, masks, rhythm_handler, words, footness=3)
+    seed = """Уж темна ночь на небеса всходила,
+		Уж в городах утих вседневный шум,
+		Луна в окно Монаха осветила. —
+		В молитвенник весь устремивший ум,
+		Панкратий наш Николы пред иконой
+		Со вздохами земные клал поклоны. —
+		Пришел Молок (так дьявола зовут),
+		Панкратия под черной ряской скрылся.
+		Святой Монах молился уж, молился,
+		Вздыхал, вздыхал, а дьявол тут как тут.
+		Бьет час, Молок не хочет отцепиться,
+		Бьет два, бьет три – нечистый всё сидит.
+		«Уж будешь мой», – он сам с собой ворчит."""
+    seed = seed.lower()
+    seed = lang.clean_text(seed)
+    result = generate_sequence(model, 32, seed, 4, masks, rhythm_handler, words, footness=4,
+                               rhyme_module=4, rhyme_dict={0: -1, 1: -1, 2: 0, 3: 1})
     print(result)
 
