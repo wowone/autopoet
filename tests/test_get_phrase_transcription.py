197ee7f6e9136156a9f552a800f2f4920db446e3
Vladimir Kukushkin <kukushkin.v@gmail.com>
2020-04-04 11:33:46 +0300

    Added simple rhyme score based on Jaccard phoneme distance

--- tests/test_get_phrase_transcription.py
+++ tests/test_get_phrase_transcription.py
@@ -3,6 +3,5 @@ from .. import lingtools
 
 def test_transcription():
     lt = lingtools.LingTools()
-    assert lt.get_phrase_transcription("под") == "пот"
-    assert lt.get_phrase_transcription("под сосной") == "път сасноṷ"
-    assert lt.get_phrase_transcription("под сосной") == "път сасноṷ"
+    assert lt.get_phrase_transcription("под", as_string=True) == "пот"
+    assert lt.get_phrase_transcription("под сосной", as_string=True) == "път сасноṷ"
