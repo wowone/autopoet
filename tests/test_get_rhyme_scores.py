6dd6ffed35701980c0ee519c333b1b06e0d72340
Gor1x <ea-post134@yandex.ru>
2020-04-10 16:55:01 +0300

    Changed right answer in rhyme test because syllabes split became better

--- tests/test_get_rhyme_scores.py
+++ tests/test_get_rhyme_scores.py
@@ -19,5 +19,6 @@ def test_transcription():
     res = pd.DataFrame(res, columns=['word0', 'word1', 'transcription0', 'transcription1', 'score'])
     res = res.sort_values('score')
 
-    assert res['word1'].to_list() == ['борода', 'провода', 'вражда', 'борта', 'вождя', 'ладах', 'морс']
-
+    #print(res['word1'].to_list())
+    #print(['борода', 'провода', 'вражда', 'борта', 'вождя', 'ладах', 'морс'])
+    assert res['word1'].to_list() == ['борода', 'провода', 'борта', 'ладах', 'вражда', 'вождя', 'морс']
