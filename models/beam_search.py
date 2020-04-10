7b32dc8b4e6ef1ee719f72ebb274085abd40c8a4
pavlov200912 <pavlov200912@gmail.com>
2020-04-10 21:54:22 +0300

    line index and stress counting delegated to beams

--- models/beam_search.py
+++ models/beam_search.py
@@ -56,6 +56,8 @@ class SeqGenerator:
         self.beam_size = beam_size
         self.temperature = temperature
         self.neutral_mask = np.array([1] * self.dict_len)
+        self.line_size = -1
+        self.footness = -1
 
     def generate_probas(self, preds):
         # exp^(log(prob) / T) / (sum (exp))
@@ -73,19 +75,19 @@ class SeqGenerator:
         # Runs once in generate_poem function
         start_beam = Beam(self)
         start_beam.fit(self.seed)
-        start_beam.step(line=0, is_final=False)
+        start_beam.step()
         self.beams = [start_beam]
         return list(map(lambda x: (0, x), start_beam.weights))
 
-    def __all_beams_step(self, line, is_final):
+    def __all_beams_step(self):
         weights = []
         for i, beam in enumerate(self.beams):
-            beam.step(line, is_final)
+            beam.step()
             beam_weights = beam.weights
             weights += list(map(lambda x: (i, x), beam_weights))
         return weights
 
-    def __beams_reborn(self, weights, line, is_final):
+    def __beams_reborn(self, weights):
         new_beams = []
         for (i, beam_inf) in weights:
             old_beam = self.beams[i]
@@ -95,39 +97,31 @@ class SeqGenerator:
             new_beam.poem = beam_inf[0]
             new_beam.shift = old_beam.shift
             new_beam.str_poem = old_beam.str_poem
+            new_beam.line_number = old_beam.line_number
+            new_beam.line_stress_count = old_beam.line_stress_count
             # Very important! Copy value, not reference
             new_beam.last_words = old_beam.last_words.copy()
 
-            new_beam.update(line, is_final)
+            new_beam.update()
             new_beams.append(new_beam)
         return new_beams
 
     def generate_poem(self, lines_len, footness):
-        for line_index in range(lines_len):
-            stress_count = 0
-            for beam in self.beams:
-                # Set shift to zero in all beams
-                # New line, new rhythm shifting
-                beam.update_shift()
-            # TODO: What if beam choose word withoud stress?
-            while stress_count != footness:
-                # Really important for all beams, is it final word of line and rhyme needed
-                is_final = footness - 1 == stress_count
-                # line is index of current line in rhyme_dict
-                line = line_index % self.rhyme_module
-                # weights - list of all beams predictions
-                # len(weights) == beam_size * len(words)
-                weights = self.__all_beams_step(line, is_final) if self.beams else self.__create_start_beam()
+        self.footness = footness
+        self.line_size = lines_len
+        some_beam_running = True
+        while some_beam_running:
+            # weights - list of all beams predictions
+            # len(weights) == beam_size * len(words)
+            weights = self.__all_beams_step() if self.beams else self.__create_start_beam()
+            # Gen only first beam_size poems, sorted by probability
+            weights = sorted(weights, key=lambda x: x[1][1])[-self.beam_size:]
+            # Now we should create list of new, most probable beams
+            new_beams = self.__beams_reborn(weights)
+            # Lose references on old_beams, they must be deleted by GC
+            self.beams = new_beams
+            some_beam_running = any(list(map(lambda x: x.in_process, new_beams)))
 
-                # Gen only first beam_size poems, sorted by probability
-                weights = sorted(weights, key=lambda x: x[1][1])[-self.beam_size:]
-                # Now we should create list of new, most probable beams
-                new_beams = self.__beams_reborn(weights, line, is_final)
-
-                # Lose references on old_beams, they must be deleted by GC
-                self.beams = new_beams
-                # TODO: is_stressed?
-                stress_count += 1
         for i, beam in enumerate(self.beams):
             print(f"Beam number {i} once wrote:")
             print(f"poem weight: {beam.beam_weight}")
@@ -146,38 +140,57 @@ class Beam:
         self.str_poem = ""
         self.weights = []
         self.beam_weight = 0
+        self.in_process = True
+        self.line_number = 0
+        self.line_stress_count = 0
 
     def fit(self, seed):
         self.poem = seed
 
-    def step(self, line, is_final):
+    def step(self):
+        if not self.in_process:
+            return
         mask_stress = self.handler.stress_masks[self.shift]
-        if is_final:
-            mask_rhyme = rhyme_mask(self.last_words[self.handler.rhyme_dict[line]], self.handler.words)
+        if self.line_stress_count + 1 == self.handler.footness:
+            mask_rhyme = rhyme_mask(
+                self.last_words[self.handler.rhyme_dict[self.line_number % self.handler.rhyme_module]],
+                self.handler.words)
         else:
             mask_rhyme = self.handler.neutral_mask
         mask = np.multiply(mask_rhyme, mask_stress)
         encoded = self.handler.tokenizer.text_to_seq([self.poem])[0]
-        # TODO: Padded with zeros! Horrible solution! REDO!!!
+        # Padded with zero prefix if len isn't enough
+
         encoded = pad_sequences([encoded], maxlen=self.handler.seq_len, truncating='pre')
 
         prediction = self.handler.model.predict(encoded)[0]
         probas = self.handler.generate_probas(prediction)
-        self.update_weights(np.multiply(probas, mask))
+        self.__update_weights(np.multiply(probas, mask))
 
-    def update_shift(self):
+    def __new_line(self):
+        self.str_poem += '\n'
         self.shift = 0
+        self.line_stress_count = 0
+        self.line_number = (self.line_number + 1)
+        if self.line_number == self.handler.line_size:
+            self.in_process = False
 
-    def update_weights(self, probas):
+    def __update_weights(self, probas):
         next_seqs = list(map(lambda x: self.poem + ' ' + x, self.handler.words))
         next_weights = np.add(np.log(probas), np.array([self.beam_weight] * len(probas)))
         self.weights = list(zip(next_seqs, next_weights))
         self.prob_max = np.max(np.array(list(map(lambda x: x[1], self.weights))))
 
-    def update(self, line, is_final):
+    def update(self):
+        if not self.in_process:
+            return
         predicted_word = self.poem.split(' ')[-1]
         self.str_poem += predicted_word + ' '
         self.shift = self.handler.rhythm_handler.get_next_shift(predicted_word, self.shift)
-        if is_final:
-            self.last_words[line] = predicted_word
-            self.str_poem += '\n'
+        # Update iterators
+        if self.handler.rhythm_handler.get_stress_syllable(predicted_word) != -1:
+            self.line_stress_count += 1
+        if self.line_stress_count == self.handler.footness:
+            self.last_words[self.line_number % self.handler.rhyme_module] = predicted_word
+            self.__new_line()
+
